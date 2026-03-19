e5220557957253682aad368931ccacd65a908f4f
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-03-19 09:26:39 +0300

    initial commit

---
+++ html.css.js
@@ -0,0 +1,12 @@
+let life = 40;
+let gameAttemps = 4;
+
+if (life >40) { 
+    console.log("You can play");
+} else {
+    console.log("You can't play");
+}
+
+
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+let grade= 80;
+if (grade >= 90) {
+    console.log("You got an A");
+} else if (grade >= 80) {
+    console.log("You got a B");
+} else if (grade >= 70) {
+    console.log("You got a C");
+} else if (grade >= 60) {
+    console.log("You got a D");
+} else {
+    console.log("You got an F");
+}
