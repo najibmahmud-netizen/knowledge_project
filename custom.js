2b364a80bd503adb40506b34d6c2d4fc9d03bbc8
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-03-19 09:46:55 +0300

    initial commit

---
+++ custom.js
@@ -0,0 +1,8 @@
+let studentName = "John Doe";
+console.log("Hello, " + studentName + "! Welcome to the class.");
+let age = 20;
+if (age >= 18) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a minor.");
+}
---
+++ functions.js
@@ -0,0 +1,17 @@
+// let studentName = "John Doe"; 
+function greetStudent(name) {
+    console.log("hello, " + studentName + "!");
+}
+
+
+// greetStudent("Alice");
+// greetStudent("Bob");
+
+function add(a, b) {
+    return a + b;
+}
+
+let result = add(5, 10);
+console.log(result); // Output: 15
+
+
